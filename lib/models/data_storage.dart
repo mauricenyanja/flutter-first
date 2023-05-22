c092ab0b026775f54cb4df3218d490ba78182e9b
mauricenyanja <mauricee423@gmail.com>
2023-05-22 13:44:31 +0300

    updated data storage

--- lib/models/data_storage.dart
+++ lib/models/data_storage.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:encrypt/encrypt.dart';
+import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:mine/encryption_key.dart';
 import 'package:mine/models/mine.dart';
@@ -12,13 +13,19 @@ class DataStorage {
   factory DataStorage() {
     return instance;
   }
-  DataStorage._internal() {}
+
+  DataStorage._internal() {
+    // initialization logic
+  }
+
+  // Variables for encryption and decryption
   static final Key key = Key.fromBase64(
     encryptionKey,
-  );
-  static final Encrypter encrypter = Encypter(AES(key));
+  ); // This is your key, you can use whatever you want
+  static final Encrypter encrypter = Encrypter(AES(key));
   static final IV iv = IV.fromLength(16);
 
+  // Your todo file
   File todoStorage = File("");
 
   Future _loadTodoFile() async {
@@ -28,13 +35,34 @@ class DataStorage {
   }
 
   Future<void> saveTodos(List<ToDo> todos) async {
-    //list for all todos to save them
+    // Create a list for all todos to save them
     List<String> encryptedTodos = List.generate(
       todos.length,
       (index) => jsonEncode(todos[index].toEncryptJson(encrypter, iv)),
     );
-    //wrtitn to the file
-    todoStorage.writeAsStringSync('{"todos}:$encryptedTodos');
+
+    // Write to the file
+    todoStorage.writeAsStringSync('{"todos": $encryptedTodos}');
   }
-  //loading todo
+
+  Future<List<ToDo>> loadTodos() async {
+    // Make sure todo file is loaded
+    if (todoStorage.path == "") {
+      await _loadTodoFile();
+    }
+
+    List<ToDo> todos = [];
+
+    // Read the json data
+    final data = jsonDecode(todoStorage.readAsStringSync());
+    List encryptedTodos = data["todos"];
+
+    // Iterate over each todo and convert it to the todo class
+    for (int i = 0; i < encryptedTodos.length; i++) {
+      Map<String, dynamic> json = encryptedTodos[i];
+      todos.add(ToDo.fromJson(json, encrypter, iv));
+    }
+
+    return todos;
+  }
 }
