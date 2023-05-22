a51353e6e5f869cc327046f23f297ba3dff7b775
mauricenyanja <mauricee423@gmail.com>
2023-05-22 13:48:27 +0300

    created todo class

---
+++ lib/models/mine.dart
@@ -0,0 +1,14 @@
+import 'package:encrypt/encrypt.dart';
+
+class ToDo {
+  final String title;
+  final String content;
+  final bool completed;
+  final DateTime from;
+
+  ToDo({
+    required this.title,
+    required this.content,
+    required this.completed,
+    required this.from,
+  });
